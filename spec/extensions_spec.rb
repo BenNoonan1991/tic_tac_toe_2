a2f8c95997d335ccfead29da0135c19f784feb0f
BenNoonan1991 <ben.noonan2@gmail.com>
2017-07-10 11:29:17 +0100

    All_empty? returns false if array is not completely empty

--- spec/extensions_spec.rb
+++ spec/extensions_spec.rb
@@ -6,5 +6,9 @@ describe Array do
     it "returns true if the array is completely empty" do
       expect(["", "", ""].all_empty?).to be_truthy
     end
+
+    it "returns false if the array is not completely empty" do
+      expect(["", 1, "", Object.new, :a].all_empty?).to be_falsey
+    end
   end
 end
